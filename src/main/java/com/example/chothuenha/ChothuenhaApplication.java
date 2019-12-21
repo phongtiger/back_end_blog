5ce9316efc931d4ee8c6edbd00f904084e382b3b
phongtiger <phongken94@gmail.com>
2019-12-21 10:30:55 +0700

    bat dau code tren devtest

---
+++ src/main/java/com/example/chothuenha/ChothuenhaApplication.java
@@ -0,0 +1,13 @@
+package com.example.chothuenha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChothuenhaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChothuenhaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chothuenha/ChothuenhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chothuenha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChothuenhaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
