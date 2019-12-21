930383ae429c245abfee6861aaf669abdbc8009c
phongtiger <phongken94@gmail.com>
2019-12-21 22:38:43 +0700

    fix lỗi thiếu anotation Component

--- src/main/java/com/example/chothuenha/message/response/JwtResponse.java
+++ src/main/java/com/example/chothuenha/message/response/JwtResponse.java
@@ -9,7 +9,11 @@ public class JwtResponse {
     private String type = "Bearer";
     private String username;
     private Collection<? extends GrantedAuthority> authorities;
-
+    public JwtResponse(String accessToken, String username, Collection<? extends GrantedAuthority> authorities) {
+        this.token = accessToken;
+        this.username = username;
+        this.authorities = authorities;
+    }
     public String getToken() {
         return token;
     }
